b32db6d9b92f1f59b3f72291c9ab577c83307c27
thanhnguyen98 <ngtruongthanh98@gmail.com>
2022-01-03 17:47:52 +0700

    Create general screen

---
+++ lib/screens/locations/locations.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../../app.dart';
+import '../../models/location.dart';
+
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Locations'),
+      ),
+      body: ListView(
+        children: locations
+            .map((location) => Text(location.name))
+            .toList(),
+      ),
+    );
+  }
+}
